6b0bf9e6a151aaf8a3b90d0ca76e599dfc239946
Administrator <1234567>
2021-02-27 10:01:30 +0800

    weixin

--- test_send.py
+++ test_send.py
@@ -1,5 +1,5 @@
 import requests
 # data = {'dialog_name': 'Python开发_通知专用', 'message': 'hello机器人'}
-data = {'dialog_name': "文件传输助手", 'message': 'hell11o'}
+data = {'dialog_name': "文件传输助手", 'message': 'hello python'}
 res = requests.post('http://192.168.8.140:99/send_message', data=data)
 print(res.json())
